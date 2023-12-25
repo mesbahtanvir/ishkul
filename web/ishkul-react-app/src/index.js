ef02a357d7f39159e4e894a29ddc1c270868ea1b
mesbahtanvir <mesbah.tanvir.cs@gmail.com>
2023-12-25 04:01:13 -0500

    add prettier and format js code

--- web/ishkul-react-app/src/index.js
+++ web/ishkul-react-app/src/index.js
@@ -1,20 +1,18 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App';
-import ReactGA from 'react-ga';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import ReactGA from "react-ga";
 
 // Create a root.
-const root = createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById("root"));
 
-
-ReactGA.initialize('G-E1NGLSXMG9');
+ReactGA.initialize("G-E1NGLSXMG9");
 ReactGA.pageview(window.location.pathname + window.location.search);
-
 
 // Initial render
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
