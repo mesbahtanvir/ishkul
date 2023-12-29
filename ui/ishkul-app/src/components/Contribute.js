02d2c926551cc497cb7150d07f20bcc9d7e88613
mesbahtanvir <mesbah.tanvir.cs@gmail.com>
2023-12-29 03:12:52 -0500

    upload

--- ui/ishkul-app/src/components/Contribute.js
+++ ui/ishkul-app/src/components/Contribute.js
@@ -1,34 +1,8 @@
 import * as React from "react";
-import { styled } from "@mui/material/styles";
-import Button from "@mui/material/Button";
-import CloudUploadIcon from "@mui/icons-material/CloudUpload";
 import Container from "@mui/material/Container";
 import CssBaseline from "@mui/material/CssBaseline";
 import { StyledBox } from "./ProfileComponents";
-const VisuallyHiddenInput = styled("input")({
-  clip: "rect(0 0 0 0)",
-  clipPath: "inset(50%)",
-  height: 1,
-  overflow: "hidden",
-  position: "absolute",
-  bottom: 0,
-  left: 0,
-  whiteSpace: "nowrap",
-  width: 1,
-});
-
-function InputFileUpload() {
-  return (
-    <Button
-      component="label"
-      variant="contained"
-      startIcon={<CloudUploadIcon />}
-    >
-      Upload file
-      <VisuallyHiddenInput type="file" />
-    </Button>
-  );
-}
+import InputFileUpload from "./Fileupload";
 
 export default function Contribute() {
   return (
